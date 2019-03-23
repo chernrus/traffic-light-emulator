5bf563f64ca88ea389467fe70baddef19db6b3c1
Ruslan Chernov <chernruslan@gmail.com>
2019-03-23 19:55:47 +0300

    create vue project

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // {
+      //   test: /\.js$/,
+      //   loader: 'babel-loader'
+      // },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    // убедитесь что подключили плагин!
+    new VueLoaderPlugin()
+  ]
+}
