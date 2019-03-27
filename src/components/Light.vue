a887df184b5d3d13329ab202175aded4d6c91b3d
Ruslan Chernov <chernruslan@gmail.com>
2019-03-28 01:08:55 +0300

    add component Light

---
+++ src/components/Light.vue
@@ -0,0 +1,97 @@
+<template id="light">
+  <div v-bind:class="color" class="light__signal">
+    <div v-bind:class="{ active: isActive, blink: isBlink }" class="light__dim-layer"></div>
+    <span v-if="isActive" v-bind:class="{ 'text-blink': isBlink }" class="light__timer">{{ time }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Light',
+    props: {
+      color: {
+        type: String
+      },
+      isActive: {
+        type: Boolean
+      },
+      time: {
+        type: Number
+      },
+      isBlink: {
+        type: Boolean
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .light__signal {
+    width: 100px;
+    height: 100px;
+    background-color: #fff;
+    border-radius: 50px;
+    position: relative;
+  }
+
+  .red {
+    background-color: #D03526;
+    color: #D03526;
+  }
+
+  .yellow {
+    background-color: #FFB321;
+    color: #FFB321;
+  }
+
+  .green {
+    background-color: #A6CE39;
+    color: #A6CE39;
+  }
+
+  .light__dim-layer {
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+    background-color: #000;
+    opacity: 0.7;
+  }
+
+  .active {
+    opacity: 0;
+  }
+
+  .light__timer {
+    font-size: 60px;
+    line-height: 100px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    color: #fff;
+  }
+
+  .text-blink {
+    animation: textBlink 2s infinite;
+  }
+
+  @keyframes textBlink {
+    /* 0% { color: inherit; }
+    25% { color: #000 }
+    50% { color: inherit; }
+    75% { color: #000 }
+    100% { color: inherit; } */
+  }
+
+  .blink {
+    animation: blink 2s infinite;
+  }
+
+  @keyframes blink {
+    0% { opacity: 0.7 }
+    25% { opacity: 0.1 }
+    50% { opacity: 0.7 }
+    75% { opacity: 0.1 }
+    100% { opacity: 0.7 }
+  }
+</style>
--- src/components/TrafficLight.vue
+++ src/components/TrafficLight.vue
@@ -1,46 +1,40 @@
 <template id="traffic-light-template">
   <div class="traffic-light">
-    <div class="traffic-light__signal red-signal">
-      <div v-if="currentLight !== 1" class="traffic-light__dim-layer">
-
-      </div>
-      <LightTimer
-        v-if="currentLight === 1"
-        v-bind:startTime="startTime"
-        v-on:onTimeout="changeLight"
-        />
-    </div>
-    <div class="traffic-light__signal yellow-signal">
-      <div v-if="currentLight !== 2" class="traffic-light__dim-layer"></div>
-      <LightTimer
-        v-if="currentLight === 2"
-        v-bind:startTime="startTime"
-        v-on:onTimeout="changeLight"
-        />
-    </div>
-    <div class="traffic-light__signal green-signal">
-      <div v-if="currentLight !== 3" class="traffic-light__dim-layer"></div>
-      <LightTimer
-        v-if="currentLight === 3"
-        v-bind:startTime="startTime"
-        v-on:onTimeout="changeLight"
-        />
-    </div>
+    <Light
+      :color="'red'"
+      :isActive="redIsActive"
+      :time="redIsActive ? time : null"
+      :isBlink="redIsActive ? isBlink : false"/>
+    <Light
+      :color="'yellow'"
+      :isActive="yellowIsActive"
+      :time="yellowIsActive ? time : null"
+      :isBlink="yellowIsActive ? isBlink : false"/>
+    <Light
+      :color="'green'"
+      :isActive="greenIsActive"
+      :time="greenIsActive ? time : null"
+      :isBlink="greenIsActive ? isBlink : false"/>
   </div>
 </template>
 
 <script>
-import LightTimer from '@/components/LightTimer';
+import Light from '@/components/Light';
 export default {
   name: 'TrafficLight',
   components: {
-    LightTimer
+    Light
   },
   data: function() {
     return {
       currentLight: 1,
-      prevLight: 1,
-      timer: null
+      prevLight: null,
+      time: 10,
+      timer: null,
+      redIsActive: true,
+      yellowIsActive: false,
+      greenIsActive: false,
+      isBlink: false
     }
   },
   props: {
@@ -49,22 +43,57 @@ export default {
       default: 1
     },
     startTime: {
-      type: Number,
-      default: 10
+      type: Number
     }
   },
   created: function() {
     this.currentLight = this.lightNumber;
+    this.prevLight = this.prevLight || this.getRandomPrevLight([1, 3]);
+
+    this.setActiveLight(this.currentLight);
+    this.startTimer();
   },
   watch: {
-    '$route': function (page) {
+    '$route': function () {
       this.prevLight = this.currentLight;
       this.currentLight = this.lightNumber;
+      this.time = this.startTime;
+      this.isBlink = false;
+      this.setActiveLight(this.currentLight);
+      this.startTimer();
     }
   },
   methods: {
+    getRandomPrevLight: function(_arr) {
+      var rand = Math.floor(Math.random() * _arr.length);
+
+      return _arr[rand];
+    },
+    setActiveLight: function(page) {
+      switch (page) {
+        case 1:
+          this.redIsActive = true;
+          this.yellowIsActive = false;
+          this.greenIsActive = false;
+          break;
+        case 2:
+          this.redIsActive = false;
+          this.yellowIsActive = true;
+          this.greenIsActive = false;
+          this.isBlink = true;
+          break;
+        case 3:
+          this.redIsActive = false;
+          this.yellowIsActive = false;
+          this.greenIsActive = true;
+          break;
+        default:
+          this.redIsActive = true;
+          this.yellowIsActive = false;
+          this.greenIsActive = false;
+      }
+    },
     changeLight: function() {
-      if(this.timer) clearTimeout(this.timer);
 
       if(this.currentLight === 1 || this.currentLight === 3) {
         this.prevLight = this.currentLight;
@@ -83,8 +112,23 @@ export default {
         this.$router.replace(`/3`);
       }
     },
-    someF: function(params) {
-      console.log('asdasd' + params);
+    startTimer: function() {
+      this.time = this.startTime;
+      if(this.timer) clearInterval(this.timer);
+
+      this.timer = setInterval(this.setCurrentTime, 1000);
+    },
+    setCurrentTime: function() {
+      if(this.time === 3) {
+        this.isBlink = true;
+      }
+      if(this.time > 1) {
+        this.time--;
+      }
+      else {
+        clearInterval(this.timer);
+        this.changeLight();
+      }
     }
   }
 }
@@ -101,32 +145,5 @@ export default {
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
-  }
-
-  .traffic-light__signal {
-    width: 100px;
-    height: 100px;
-    background-color: #fff;
-    border-radius: 50px;
-  }
-
-  .red-signal {
-    background-color: #D03526;
-  }
-
-  .yellow-signal {
-    background-color: #FFB321;
-  }
-
-  .green-signal {
-    background-color: #A6CE39;
-  }
-
-  .traffic-light__dim-layer {
-    width: 100%;
-    height: 100%;
-    border-radius: 100%;
-    background-color: #000;
-    opacity: 0.7;
   }
 </style>
